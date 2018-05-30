bfffa8aca404a1070c9e13d0854e70e7d791bc75
Taeyun Kim <taeyun1010@gmail.com>
2018-05-30 12:57:20 +0900

    no thread complete

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ storagepath = "tempstorage/"
 # TODO
 directorypath = "C:/Users/User/Desktop/test_coins/c/"
 
+finalresultpath = "finalresult2.txt"
+
 r = 2
 
 def rSubset(arr, r):
@@ -96,124 +98,177 @@ if __name__ == '__main__':
     coins = get_immediate_subdirectories(directorypath)
 
     combi = rSubset(coins, r)
-    
-    for thiscombi in combi:
-        coin1 = thiscombi[0]
-        coin2 = thiscombi[1]
-        copyFiles(os.path.join(directorypath, coin1), 1)
-        copyFiles(os.path.join(directorypath, coin2), 2)
-        
-        # TODO
-#         call(["java", "-jar", "C:\Users\User\Desktop\jplag.jar", "-l", "c/c++", "-m", "0%", "-r", "C:/Users/User/Desktop/tempresults", "C:/Users/User/Desktop/nbvcxz2/jplagRunner/tempstorage"])
-        proc = subprocess.Popen(["java", "-jar", "C:\Users\User\Desktop\jplag.jar", "-l", "c/c++", "-m", "0%", "-vq", "-r", "C:/Users/User/Desktop/tempresults", "C:/Users/User/Desktop/nbvcxz2/jplagRunner/tempstorage"], stdout=subprocess.PIPE)
-        
-        
-        
-#         #
-#         #
-#         poll_obj = select.poll()
-#         poll_obj.register(proc.stdout, select.POLLIN) 
-#         outputline =proc.stdout.readline()
-#         while(outputline):
-#             outputline =proc.stdout.readline()
-#             print(outputline)
-#         #
-#         #
-        
-        
-        
-        
-#         #
-#         #
-#         while True:
-#             line = proc.stdout.readline()
-#             print(line)
-#             if not line: break
-#         #
-#         #
-        
-        
-        
-        
-#         while True:
-#             line = proc.stdout.readline()
-#             if (" submissions" in line):
-#                 break
-#         
-#         
-#         while(True):
-#             line = run_with_timeout(10, None, proc.stdout.readline)
-#             if line is None:
-#                 break
-#             else:
-#                 print(line)
-        
-        
-        
-        
-        
-        
-         
-        output = proc.stdout.read()
-        index = output.find("Comparing ")
-        output = output[index:]
-        buf = StringIO.StringIO(output)
-        
-        # total sum of percetages , to be divided by count
-        sumpercentage = 0
-        count = 0
-        firstfile = ""
-        percentage = 0
-        #max percentage encountered for this file
-        maxpercentage = 0
-        #get the first percentage and first firstfile
-        for line in buf:
-            firstfile = getFirstFile(line)
-            percentage = getPercentage(line)
-            count = count + 1
-            break
-        
-        for line in buf:
-#             print(line)
-#             print(getFirstFile(line))
-#             print(getPercentage(line))
-            thisfile = getFirstFile(line)
-            thispercentage = getPercentage(line)
+    with open(finalresultpath,"w+") as fr:
+        satisfied = 0
+        for thiscombi in combi:
+            coin1 = thiscombi[0]
+            coin2 = thiscombi[1]
+            coin1path = os.path.join(directorypath, coin1)
+            coin2path = os.path.join(directorypath, coin2)
             
-            #new file, initialize the variables
-            if (thisfile != firstfile):
-                firstfile = thisfile
-                sumpercentage = sumpercentage + maxpercentage
-                maxpercentage = 0
+            
+            
+            if (satisfied == 0):
+                if(not((coin1 == "adshares-esc-archive-master") and (coin2 == "bitshares-bitshares-core-archive-master"))):
+                    continue
+                else:
+                    satisfied = 1
+                    
+            # get the size of directory
+            total_size1 = 0
+            for path, dirs, files in os.walk(coin1path):
+                for f in files:
+                    fp = os.path.join(path, f)
+                    total_size1 += os.path.getsize(fp)
+            
+            if ((total_size1 == 0) or (total_size1 > 10000000 )):
+                continue
+            
+            # get the size of directory
+            total_size2 = 0
+            for path, dirs, files in os.walk(coin2path):
+                for f in files:
+                    fp = os.path.join(path, f)
+                    total_size2 += os.path.getsize(fp)
+            
+            if ((total_size2 == 0) or (total_size2 > 10000000 )):
+                continue
+            
+            copyFiles(coin1path, 1)
+            copyFiles(coin2path, 2)
+            
+            # TODO
+    #         call(["java", "-jar", "C:\Users\User\Desktop\jplag.jar", "-l", "c/c++", "-m", "0%", "-r", "C:/Users/User/Desktop/tempresults", "C:/Users/User/Desktop/nbvcxz2/jplagRunner/tempstorage"])
+            
+            proc = subprocess.Popen(["java", "-jar", "C:\Users\User\Desktop\jplag.jar", "-l", "c/c++", "-m", "0%", "-vq", "-r", "C:/Users/User/Desktop/tempresults", "C:/Users/User/Desktop/nbvcxz2/jplagRunner/tempstorage"], stdout=subprocess.PIPE)
+#             proc = subprocess.Popen(["java", "-jar", "C:\Users\User\Desktop\jplag.jar", "-l", "c/c++", "-m", "0%", "-vq", "-r", "C:/Users/User/Desktop/tempresults", "C:/Users/User/Desktop/jplagexercise1/temp1/folder1"], stdout=subprocess.PIPE)
+            
+            
+    #         #
+    #         #
+    #         poll_obj = select.poll()
+    #         poll_obj.register(proc.stdout, select.POLLIN) 
+    #         outputline =proc.stdout.readline()
+    #         while(outputline):
+    #             outputline =proc.stdout.readline()
+    #             print(outputline)
+    #         #
+    #         #
+            
+            
+            
+            
+    #         #
+    #         #
+    #         while True:
+    #             line = proc.stdout.readline()
+    #             print(line)
+    #             if not line: break
+    #         #
+    #         #
+            
+            
+            
+            
+    #         while True:
+    #             line = proc.stdout.readline()
+    #             if (" submissions" in line):
+    #                 break
+    #         
+    #         
+    #         while(True):
+    #             line = run_with_timeout(10, None, proc.stdout.readline)
+    #             if line is None:
+    #                 break
+    #             else:
+    #                 print(line)
+            
+            
+            
+            
+            
+            
+             
+            output = proc.stdout.read()
+            index = output.find("Comparing ")
+            output = output[index:]
+            buf = StringIO.StringIO(output)
+            
+            # total sum of percetages , to be divided by count
+            sumpercentage = 0
+            count = 0
+            firstfile = ""
+            percentage = 0
+            #max percentage encountered for this file
+            maxpercentage = 0
+            #get the first percentage and first firstfile
+            for line in buf:
+                #if this line does not contain files from both coins, skip
+                if (not(("1_" in line) and ("2_" in line))):
+                    continue
+                firstfile = getFirstFile(line)
+                percentage = getPercentage(line)
                 count = count + 1
+                break
             
-            if (thispercentage > maxpercentage):
-                maxpercentage = thispercentage
-            
-        if (count != 0):
-#             fr.write("Similarity percentage between " + coin1 + " and " + coin2 + " = " + str(((sumPercentage1 / numFiles1) + (sumPercentage2 / numFiles2)) / 2) + "\n")
-#             fr.flush()
-            print("Similarity percentage between " + coin1 + " and " + coin2 + " = " + str((sumpercentage / count)) + "\n")
-        else:
-            print("Similarity percentage between " + coin1 + " and " + coin2 + " = " + "count was zero!!!!!!! no compared result \n")
-        
-            
-            
+            lastpercentageAdded = 0
+                    
+            lines = buf.readlines()
+            temp = xrange(len(lines))
+            for i in xrange(len(lines)):
+    #             print(line)
+    #             print(getFirstFile(line))
+    #             print(getPercentage(line))
+                line = lines[i]
+                thisfile = getFirstFile(line)                
+                #if this line does not contain files from both coins, skip
+                if (not(("1_" in line) and ("2_" in line))):
+                    continue
+    
+                thispercentage = getPercentage(line)
+                
+                #new file, initialize the variables
+                if (thisfile != firstfile):
+                    firstfile = thisfile
+    #                 sumpercentage = sumpercentage + maxpercentage
+                    maxpercentage = thispercentage
+                    count = count + 1
+                
+                if (thispercentage > maxpercentage):
+                    maxpercentage = thispercentage
+                
+                # consider next line only if there is next line
+                if (i == (len(lines) - 1)):
+                    sumpercentage = sumpercentage + maxpercentage
+                    break
+                nextline = lines[(i+1)]
+                if (getFirstFile(nextline) != thisfile):
+                    sumpercentage = sumpercentage + maxpercentage
+                
+            if (count != 0):
+    #             fr.write("Similarity percentage between " + coin1 + " and " + coin2 + " = " + str(((sumPercentage1 / numFiles1) + (sumPercentage2 / numFiles2)) / 2) + "\n")
+    #             fr.flush()
+                fr.write("Similarity percentage between " + coin1 + " and " + coin2 + " = " + str((sumpercentage / count)) + "\n")
+                fr.flush()
+            else:
+                fr.write("Similarity percentage between " + coin1 + " and " + coin2 + " = " + "count was zero!!!!!!! no compared result \n")
+                fr.flush()
+                
+                
+                 
+                
+                
+                
+                
+    #         print(output)
+    #         for line in output:
+    #             print(line)
+    #             print("printed line")
              
             
             
+            emptyfolder(storagepath)
+            emptyfolder("C:/Users/User/Desktop/tempresults/")
             
-            
-#         print(output)
-#         for line in output:
-#             print(line)
-#             print("printed line")
-         
-        
-        
-        emptyfolder(storagepath)
-        emptyfolder("C:/Users/User/Desktop/tempresults/")
-        break
-        
         
         
---
+++ tempstorage/2_britcoin3.2-master_src_checkblocks.h
@@ -0,0 +1,19 @@
+#ifndef CHECKBLOCKS_H
+#define CHECKBLOCKS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+static const uint256 hashGenesisBlock("00000c2cafe160374ffe5b516a2d2c83d7d13cf4e910dee00dd4fc2f7fa9c7cf");
+static const uint256 hashGenesisBlockTestNet("000018950ec725ebb4e4537bbb501fb0b1774436c9e90dcea12b2ea42c717a99");
+
+// Chechpoints to ensure the correct chain usage during sync
+
+static const uint256 CheckBlock1 ("3a9d79b62df24e0560ba1907a82e2f8bd6a20d66dadfe4a2bb9b1cc41d770f9c"); // Checkpoint at block 977,750
+static const uint256 CheckBlock2 ("be819fa31fc2ed753206c911ea899a23430aa645b768ff3ae72f7cfb4159ba97"); // Checkpoint at block 1,028,500
+
+// Testnet Checkpoints
+// none presently
+
+
+#endif // CHECKBLOCKS_H
